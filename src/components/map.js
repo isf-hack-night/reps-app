9c659c7cf6fe70b41e1e4125913764c4840fd5df
mzwemer <mzwemer@users.noreply.github.com>
2017-09-03 13:34:11 -0700

    fix #29

--- src/components/map.js
+++ src/components/map.js
@@ -38,7 +38,7 @@ class JustMap extends Component {
     this.state.markers.clearLayers()
     this.state.upperDistricts.clearLayers()
     this.state.lowerDistricts.clearLayers()
-    this.state.layerControl.getContainer.hide()
+   // this.state.layerControl.getContainer.hide()
     this.state.map.flyToBounds(STATE_BOUNDS)
   // document.getElementById('autocomplete').value = '';
   }
@@ -71,25 +71,33 @@ class JustMap extends Component {
     } else {
       this.state.upperDistricts.clearLayers()
       this.state.lowerDistricts.clearLayers()
-      this.state.layerControl.getContainer.hide()
+     // this.state.layerControl.getContainer.hide()
 
     }
   }
 
   zoomDistrict (districtData) {
-    const bbox = districtData.upper.bbox  //TODO get bbox of both bboxes
+    const bboxU = districtData.upper.bbox  //TODO get bbox of both bboxes
+    const bboxL = districtData.lower.bbox 
+  
+    const bbox = bboxU
+    bboxU[0][0] = Math.min(bboxU[0][0],bboxL[0][0])
+    bbox[0][1]  = Math.min(bboxU[0][1],bboxL[0][1])
+    bbox[1][0]  = Math.max(bboxU[1][0],bboxL[1][0])
+    bbox[1][1]  = Math.max(bboxU[1][1],bboxL[1][1])
+
 
     const drawNewDistrict = true
     this.state.map.flyToBounds(bbox)
 
     this.state.upperDistricts.clearLayers()
     this.state.lowerDistricts.clearLayers()
-    this.drawDistrict(districtData.upper, 'upper')
-    this.drawDistrict(districtData.lower, 'lower')
-    this.state.layerControl.getContainer().show()
+    this.drawDistrict(districtData.upper)
+    this.drawDistrict(districtData.lower)
+   // this.state.layerControl.getContainer().show()
   }
 
-  drawDistrict (district, layer) {
+  drawDistrict (district) {
     let shape = []
     for (let a in district.shape) {
       shape[a] = []
@@ -104,9 +112,8 @@ class JustMap extends Component {
       }
     }
 
-    console.log(district)
-    const districtColor = layer === 'upper' ? COLORS.DISTRICT.UPPER : COLORS.DISTRICT.LOWER
-    console.log(districtColor)
+
+    const districtColor = district.chamber === 'upper' ? COLORS.DISTRICT.UPPER : COLORS.DISTRICT.LOWER
 
     for (let i = 0; i < shape.length; i++) { 
       const boundary = shape[i][0].slice(1).map(x => [x[1], x[0]] )  //assumes no donuts
@@ -114,7 +121,7 @@ class JustMap extends Component {
     }
 
     const polygon = L.polygon(shape, { color: districtColor })
-    if( layer == 'upper')
+    if( district.chamber == 'upper')
       this.state.upperDistricts.addLayer( polygon )
     else 
       this.state.lowerDistricts.addLayer( polygon )
@@ -123,7 +130,6 @@ class JustMap extends Component {
 
   componentDidMount () {
 
-    console.log("MAP DID MOUNT")
     // aka init map
     L.mapbox.accessToken = API_KEYS.mapbox
     const map = L.mapbox.map('map', 'mapbox.light')
@@ -137,19 +143,18 @@ class JustMap extends Component {
     map.addLayer( upperDistricts)
     map.addLayer(lowerDistricts)
 
-
     const overlayHTMLUpper = "<span style='color:" + COLORS.DISTRICT.UPPER + "''>State Senate Districts</span>"
     const overlayHTMLLower = "<span style='color: " + COLORS.DISTRICT.UPPER + "'>State Assembly Districts</span>"
 
     //TODO update colors here as wll
     const overlayMaps = {
        overlayHTMLUpper : upperDistricts,
-       overlayHTMLLower: lowerDistricts
+       overlayHTMLLower : lowerDistricts
     };
 
     const layerControl = L.control.layers(null, overlayMaps, {collapsed:false})
-    layerControl.getContainer().hide()
     layerControl.addTo(map);
+    //layerControl.hide()
 
     map.on('click', this.handleClick)
 
