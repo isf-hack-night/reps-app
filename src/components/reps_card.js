1f2880f6671b76055ff75bdbccbc90231d03a0c7
mzwemer <mzwemer@users.noreply.github.com>
2017-09-03 16:35:08 -0700

    reps card styling

--- src/components/reps_card.js
+++ src/components/reps_card.js
@@ -8,17 +8,19 @@ class RepsCardBase extends Component {
     if (!rep) { return }
 
     const houseTitle = rep.chamber === 'H' ? 'State Assemblymember' : 'State Senator'
+    const houseColor = rep.chamber === 'H' ?  COLORS.DISTRICT.LOWER : COLORS.DISTRICT.UPPER
     const partyColor = rep.partyCode === 'D' ? COLORS.TEXT.BLUE : COLORS.TEXT.RED
+
     return (
-      <div className="RepsCard" onClick={this.props.goToRepPage}>
+      <div className="RepsCard" onClick={this.props.goToRepPage} style={`border: 2px solid ${houseColor};`}>
         <h5>Your {houseTitle} is:</h5>
         <div>
-          <img src={CAP_PIC}/>
+            <img  src={CAP_PIC} style={`border: 1px solid ${partyColor};`}/>
           <div className="RepsCard-info">
             <p>{rep.prefix} {rep.legalName} (<span style={`color: ${partyColor};`}>{rep.partyCode}</span>)</p>
             <p>District: {rep.district}</p>
             <p>Phone: {rep.offices[0].phone}</p>
-            <a href={`mailto:${rep.email}`}>{rep.email}</a>
+            <a href={`mailto:${rep.email}`}>Email</a>
           </div>
         </div>
       </div>
