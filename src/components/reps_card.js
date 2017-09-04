3d2e7b578700873b7ff08d20fffe6d0a1f8c8ea6
mzwemer <mzwemer@users.noreply.github.com>
2017-09-03 17:21:04 -0700

    cleaned up RepsCard

--- src/components/reps_card.js
+++ src/components/reps_card.js
@@ -11,22 +11,39 @@ class RepsCardBase extends Component {
     const houseColor = rep.chamber === 'H' ?  COLORS.DISTRICT.LOWER : COLORS.DISTRICT.UPPER
     const partyColor = rep.partyCode === 'D' ? COLORS.TEXT.BLUE : COLORS.TEXT.RED
 
+    console.log(rep)
+
     return (
-      <div className="RepsCard" onClick={this.props.goToRepPage} style={`border: 2px solid ${houseColor};`}>
-        <h5>Your {houseTitle} is: {rep.prefix} {rep.legalName} (<span style={`color: ${partyColor};`}>{rep.partyCode}</span>)</h5>
+      <div className="RepsCard" onClick={this.props.goToRepPage} style={`border: 3px solid ${houseColor};`}>
+        <h5>Your {houseTitle} is: </h5>
+        <h4> {rep.legalName} (<span style={`color: ${partyColor};`}>{rep.partyCode}</span>)</h4>
         <div>
+          <div className="RepImageWrapper">
             <img  src={CAP_PIC} style={`border: 1px solid ${partyColor};`}/>
+
+          </div>
           <div className="RepsCard-info">
-            
             <p>District: {rep.district}</p>
-            <p>Phone: {rep.offices[0].phone}</p>
-            <a href={`mailto:${rep.email}`}>Email</a>
+            <p><i class="fa fa-phone fa-fw" aria-hidden="true"></i> {rep.offices[0].phone}</p>
+            <div className= "RepSocialWrapper">
+              <a href={`${rep.website}`} alt="Homepage"><i class="fa fa-home fa-fw" aria-hidden="true"></i></a>
+              <a href={`${rep.twitter}`} alt="Twitter"><i class="fa fa-twitter fa-fw" aria-hidden="true"></i></a>
+              <a href={`mailto:${rep.email}`} alt="Email"><i class="fa fa-envelope fa-fw" aria-hidden="true"></i></a>
+            </div>
+
           </div>
         </div>
       </div>
     )
   }
 }
+
+
+            //commented out until read to populate with links
+            //<a href={`mailto:${rep.email}`} ><i class="fa fa-envelope-square" aria-hidden="true" ></i></a>
+            //<i class="fa fa-twitter-square" aria-hidden="true"></i>
+            //<i class="fa fa-facebook-square" aria-hidden="true"></i>
+
 
 const RepsCard = withRouter(({history, rep}) => (
   <RepsCardBase
