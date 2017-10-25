56b95d8f089a1542606e26a0ce151eba8fe34f9e
Christoph Neyer <christoph.neyer@gmail.com>
2017-10-24 22:04:49 -0700

    use constant for open states url * also semicolons are your friend

--- src/constants.js
+++ src/constants.js
@@ -1,40 +1,43 @@
 // root path to app
 
-const INJECTION_DEV_MODE = true
+const INJECTION_DEV_MODE = true;
 
-export const ROOT_PATH =  INJECTION_DEV_MODE ? '/index.php/test/' : '/'
+export const ROOT_PATH = INJECTION_DEV_MODE ? '/index.php/test/' : '/';
 
-export const QUERY_SPLIT = INJECTION_DEV_MODE ? '/index.php/test/' : '.org/'
+export const QUERY_SPLIT = INJECTION_DEV_MODE ? '/index.php/test/' : '.org/';
 
 // path to individual rep page
-export const REPS_PATH = `${ROOT_PATH}rep/`
+export const REPS_PATH = `${ROOT_PATH}rep/`;
 
 // operation state within the US
-export const US_STATE = 'ca'
+export const US_STATE = 'ca';
 
 // state center lat/lng
-export const STATE_CENTER = [37.2719, -119.2702]
+export const STATE_CENTER = [37.2719, -119.2702];
 
 // state boundaries
-export const STATE_BOUNDS = [[32.5343, -124.4096], [42.0095, -114.1308]]
+export const STATE_BOUNDS = [[32.5343, -124.4096], [42.0095, -114.1308]];
 
-export const MAP_PERCISION = 4
+export const MAP_PERCISION = 4;
 
 // amplify team base url
-export const AMPLIFY_URL_BASE = `https://civictools.appspot-preview.com/api/v1/actions?teamId=-Kr2PPG5GTkR3oFfQ_Lq&state=${US_STATE}`
+export const AMPLIFY_URL_BASE = `https://civictools.appspot-preview.com/api/v1/actions?teamId=-Kr2PPG5GTkR3oFfQ_Lq&state=${US_STATE}`;
 
-export const ACTION_TYPES = ['call' , 'flex']
+// Open states url base
+export const OPEN_STATES_URL_BASE = 'https://openstates.org/api/v1';
+
+export const ACTION_TYPES = ['call', 'flex'];
 
 // image sources
 //export const CAP_PIC = 'https://nerdist.com/wp-content/uploads/2016/05/20160506_nerdistnews_capexclusive_1x1.jpg'
-export const REP_PIC_PATH = 'https://s3-us-west-1.amazonaws.com/amplify-portraits/'
+export const REP_PIC_PATH = 'https://s3-us-west-1.amazonaws.com/amplify-portraits/';
 
 
 // actions display nums
 // number of actions to show on initial actions display list load
-export const ACTIONS_DISPLAY_LIMIT = 3
+export const ACTIONS_DISPLAY_LIMIT = 3;
 // number of additional actions to display on "load more" click
-export const ACTIONS_DISPLAY_INCREMENT = 10
+export const ACTIONS_DISPLAY_INCREMENT = 10;
 
 // potential url params to parse out of the url
 export const PARAM_TYPES = [
@@ -43,7 +46,7 @@ export const PARAM_TYPES = [
   'actionId',
   'districtLower',
   'districtUpper'
-]
+];
 
 // colors
 export const COLORS = {
@@ -52,11 +55,10 @@ export const COLORS = {
     RED: '#ff0000',
   },
   DISTRICT: {
-  	UPPER: '#fbb040',
-    LOWER: '#39b54a', 
+    UPPER: '#fbb040',
+    LOWER: '#39b54a',
   }
-
-}
+};
 
 // consistent copy
 
