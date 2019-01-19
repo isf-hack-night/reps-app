01d327c0be5ed1e1a15689f4c9f976cdca44db22
Christoph Neyer <christoph.neyer@gmail.com>
2019-01-19 15:56:25 -0800

    Handle missing window.google a bit better and hook into g-container-main

--- src/index.js
+++ src/index.js
@@ -2,15 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'app/App';
 
-
-function tryToRender () {
-  setTimeout(() => {
-    if (window.google) {
-      ReactDOM.render(<App/>, document.getElementById('reps_app_root'));
-    } else {
-      tryToRender();
-    }
-  }, 100)
-}
-
-tryToRender();
+ReactDOM.render(<App/>, document.getElementById('g-container-main'));
