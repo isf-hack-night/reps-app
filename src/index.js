56e8d27c31c2d80a559211a9a58fd4d2d817d68d
Christoph Neyer <christoph.neyer@gmail.com>
2019-02-09 11:06:36 -0800

    small tweaks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'app/App';
+import {ROOT_REACT_ELEMENT} from 'local_constants';
 
-ReactDOM.render(<App/>, document.getElementById('g-container-main'));
+const rootElement = document.getElementById(ROOT_REACT_ELEMENT);
+
+if (!rootElement) {
+  console.log('Unable to find root element for react app: ' + ROOT_REACT_ELEMENT);
+} else {
+  ReactDOM.render(<App/>, rootElement);
+}
+
